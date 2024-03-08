66e4d587e2e0a1a8937f193d523201283db34f64
Jen L <55827632+y0nza1@users.noreply.github.com>
2024-03-08 02:18:51 -0800

    Update axe_game.cpp

--- popup-window/axe_game.cpp
+++ popup-window/axe_game.cpp
@@ -5,13 +5,19 @@
 int main()
 {
     // window dimensions
-    int width{750}, height{500}; 
+    int width{800}, height{500}; 
 
     // takes 3 parameters: width and height (int) and title (string)
     InitWindow(width, height, "Jen's Window");
 
     // circle coordinates
-    int circle_x{375}, circle_y{250};
+    int circle_x{200};
+    int circle_y{200};
+    
+    // axe coordinates
+    int axe_x{400};
+    int axe_y{0};
+    int direction{10};
 
     SetTargetFPS(60); // refresh frames at this rate; prevents zoomies
 
@@ -22,27 +28,36 @@ int main()
         ClearBackground(WHITE);
 
         // game logic begins
+        DrawCircle(circle_x, circle_y, 25, GREEN);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
+        // move the axe
+        axe_y += direction;
 
-        if (IsKeyDown(KEY_D))
+        if (axe_y > 500 || axe_y < 0)
         {
-            circle_x = circle_x + 10;
+            direction = -direction;
         }
 
-        if (IsKeyDown(KEY_A))
+        // circle movements controls
+        if (IsKeyDown(KEY_D) && circle_x < 750)
         {
-            circle_x = circle_x - 10;
+            circle_x += 10;
         }
 
-        if (IsKeyDown(KEY_W))
+        if (IsKeyDown(KEY_A) && circle_x > 0)
         {
-            circle_y = circle_y - 10;
+            circle_x -= 10;
         }
 
-        if (IsKeyDown(KEY_S))
+        if (IsKeyDown(KEY_W) && circle_y > 0)
         {
-            circle_y = circle_y + 10;
+            circle_y -= 10;
+        }
+
+        if (IsKeyDown(KEY_S) && circle_y < 500)
+        {
+            circle_y += 10;
         }
        
         // end game logic 
